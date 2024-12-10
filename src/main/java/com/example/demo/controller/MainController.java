29ce289069c26694f0493b3e4fee532adbf8e181
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2024-12-10 10:48:07 +0900

    responsebody

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -2,16 +2,21 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // 점원, service: 요리사
 public class MainController {
     @GetMapping("/")
-    public void home() {
+    @ResponseBody
+    public int home() {
         System.out.println("Home Page Requested");
+        return 22;
     }
 
     @GetMapping("/about")
-    public void about() {
+    @ResponseBody
+    public String about() {
         System.out.println("about Page Requested");
+        return "안녕하세요";
     }
 }
