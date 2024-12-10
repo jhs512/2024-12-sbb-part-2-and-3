ed91dbdd54a0b8a2c39b21d9ccc8452fe0b2a540
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-10 22:15:07 +0900

    test : Custom Query findBySubjectAndContent() 활용한 Question Data 조회

--- src/test/java/com/ann/annovation/SbbApplicationTests.java
+++ src/test/java/com/ann/annovation/SbbApplicationTests.java
@@ -17,8 +17,9 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        Question q = this.questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
         assertEquals(1, q.getId());
-        // 실제 Query에서는 WHERE q1_0.subject=? 라는 조건절이 추가된다.
+        // 실제 Query에서는 WHERE q1_0.subject=? and q1_0.content=? 라는 조건절이 추가된다.
     }
 }
