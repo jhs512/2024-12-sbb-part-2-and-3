acfbd1f87027949704f6ef337dd6a68548a9466a
davinyakma <poohcdv3652@naver.com>
2024-12-10 11:05:19 +0900

    feat: ResponseBody

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,18 +2,29 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     //Get http://localhost:8080/
     @GetMapping("/")
-    public void home() {
+    @ResponseBody
+    public int home() {
         System.out.println("home");
+        return 22;
     }
 
     //Get http://localhost:8080/about
     @GetMapping("/about")
-    public void about() {
+    @ResponseBody
+    public String about() {
         System.out.println("about");
+        return "안녕하세요.";
     }
+
+    public void contact() {
+        System.out.println("contact");
+    }
+
+
 }
