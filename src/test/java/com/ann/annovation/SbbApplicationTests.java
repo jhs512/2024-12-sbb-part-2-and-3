0110bac1a52d2009a1d18fb01e34b46ad0eba448
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-10 22:01:30 +0900

    test : Question Data 저장

--- src/test/java/com/ann/annovation/SbbApplicationTests.java
+++ src/test/java/com/ann/annovation/SbbApplicationTests.java
@@ -12,10 +12,13 @@ class SbbApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
+
     @Test
     void testJpa() {
         Question q1 = new Question();
-        q1.setSubject("sbb 무엇인가요?");
+        q1.setSubject("sbb가 무엇인가요?");
         q1.setContent("sbb에 대해서 알고 싶습니다.");
         q1.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q1);  // 첫번째 질문 저장
