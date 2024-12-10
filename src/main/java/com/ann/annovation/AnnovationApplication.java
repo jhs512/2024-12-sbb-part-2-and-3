5654dc7b83f849b637b68dfedc219715756282c3
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-10 10:06:12 +0900

    initial commit

---
+++ src/main/java/com/ann/annovation/AnnovationApplication.java
@@ -0,0 +1,13 @@
+package com.ann.annovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnovationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnovationApplication.class, args);
+	}
+
+}
