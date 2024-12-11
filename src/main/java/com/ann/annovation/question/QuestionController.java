9fd52f21b42d1204b999fad48f9cf86b3588b6d3
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-11 16:45:20 +0900

    feat : QuestionService Exception 추가 & Controller를 통해 Question 객체 Template으로 전달

--- src/main/java/com/ann/annovation/question/QuestionController.java
+++ src/main/java/com/ann/annovation/question/QuestionController.java
@@ -30,6 +30,13 @@ public class QuestionController {
     }
     @GetMapping(value = "/question/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
+        // QuestionService의 getQuestion 메서드를 호출하여 특정 id에 해당하는 질문 데이터를 가져옴
+        Question question = this.questionService.getQuestion(id);
+
+        // 가져온 질문 데이터를 모델(Model)에 추가
+        // "question"이라는 이름으로 뷰에 전달하여 화면에서 사용 가능
+        // model.addAttribute("key", value)를 통해 데이터를 추가하며, 뷰에서는 key 이름으로 접근합니다.
+        model.addAttribute("question", question);
         return "question_detail";
     }
 }
