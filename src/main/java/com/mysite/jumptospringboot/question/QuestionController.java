4364dc253b3a1bebc9f311200b2d4a88bd5d1d34
k-haechan <gibbm1127@naver.com>
2024-12-13 12:52:37 +0900

    2장 스프링부트 개발 준비하기 - 2-09 서비스 활용하기

--- src/main/java/com/mysite/jumptospringboot/question/QuestionController.java
+++ src/main/java/com/mysite/jumptospringboot/question/QuestionController.java
@@ -4,19 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
-public class questionController {
+public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model){
-        List<Question> questionList = questionRepository.findAll();
+        List<Question> questionList = questionService.getList();
         model.addAttribute("questionList",questionList);
         return "question_list";
     }
