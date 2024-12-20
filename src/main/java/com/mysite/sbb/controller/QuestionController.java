12f0a3a7d6fb6bab3059be5938b17cbe57c3a678
davinyakma <poohcdv3652@naver.com>
2024-12-20 13:50:44 +0900

    feat: create question_form and overloading /create method

--- src/main/java/com/mysite/sbb/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/controller/QuestionController.java
@@ -5,9 +5,7 @@ import com.mysite.sbb.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +33,11 @@ public class QuestionController {
     @GetMapping("/create")
     public String questionCreate() {
         return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
+        // TODO 질문을 저장한다.
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
