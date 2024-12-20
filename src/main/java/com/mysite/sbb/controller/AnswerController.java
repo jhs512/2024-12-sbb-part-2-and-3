2a834492f7adb549d0489d3caf46a2480900d051
davinyakma <poohcdv3652@naver.com>
2024-12-20 11:52:16 +0900

    feat: create AnswerService to create answer

--- src/main/java/com/mysite/sbb/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/controller/AnswerController.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb.controller;
 
 import com.mysite.sbb.entity.Question;
+import com.mysite.sbb.service.AnswerService;
 import com.mysite.sbb.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -16,11 +17,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AnswerController {
 
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
         Question question = this.questionService.getQuestion(id);
-        // TODO: 답변을 저장한다.
+        this.answerService.create(question, content);
         return String.format("redirect:/question/detail/%s", id);
     }
 }
