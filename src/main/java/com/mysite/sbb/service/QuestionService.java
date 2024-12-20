a90dcf7eec4ce89860b8e99297aae1778fe14df4
davinyakma <poohcdv3652@naver.com>
2024-12-20 14:02:36 +0900

    feat: add create() method to questionService

--- src/main/java/com/mysite/sbb/service/QuestionService.java
+++ src/main/java/com/mysite/sbb/service/QuestionService.java
@@ -1,11 +1,12 @@
 package com.mysite.sbb.service;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.entity.Question;
 import com.mysite.sbb.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import com.mysite.sbb.DataNotFoundException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +27,13 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
