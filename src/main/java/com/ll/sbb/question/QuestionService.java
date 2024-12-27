4f3ef9d88aa52cce69ec14d6ad31b4284685513d
Tae-heeMin <hee66265@naver.com>
2024-12-27 21:35:30 +0900

    2-10

--- src/main/java/com/ll/sbb/question/QuestionService.java
+++ src/main/java/com/ll/sbb/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.ll.sbb.question;
 
+import com.ll.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +15,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(long id) {
+        Optional<Question> op = questionRepository.findById(id);
+        if (op.isPresent()) {
+            return op.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
