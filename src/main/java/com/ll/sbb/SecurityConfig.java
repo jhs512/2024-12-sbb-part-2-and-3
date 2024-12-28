3e4b8b7289aed76043a50a6973e10241a2f6846b
Taeheeeeeee <taeheemin62@gmail.com>
2024-12-28 21:07:43 +0900

    3-06

--- src/main/java/com/ll/sbb/SecurityConfig.java
+++ src/main/java/com/ll/sbb/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -11,6 +13,12 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
---
+++ src/main/java/com/ll/sbb/user/UserService.java
@@ -0,0 +1,22 @@
+package com.ll.sbb.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String email, String password) {
+        SiteUser siteUser = new SiteUser();
+        siteUser.setUsername(username);
+        siteUser.setEmail(email);
+        siteUser.setPassword(passwordEncoder.encode(password));
+
+        return userRepository.save(siteUser);
+    }
+}
