e4d950c8d2ab96ac1179e716f3a0bc7e662aa13a
loong6138 <loong6138@gmail.com>
2024-12-29 21:11:22 +0900

    3-06 회원 가입 기능 구현하기

---
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -0,0 +1,22 @@
+package com.mysite.sbb.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String email, String password) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password)); // 암호화 하여 비밀번호 저장
+        this.userRepository.save(user);
+        return user;
+    }
+}
