42f77fec6851d7a5254122dbb8e21c37cc9dc5ff
baekminjin <bhs3392@naver.com>
2025-01-01 22:42:21 +0900

    3-02 paging

--- src/main/java/com/mysite/sbbstudy/question/QuestionController.java
+++ src/main/java/com/mysite/sbbstudy/question/QuestionController.java
@@ -3,6 +3,7 @@ package com.mysite.sbbstudy.question;
 import com.mysite.sbbstudy.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -18,9 +19,9 @@ public class QuestionController {
 	private final QuestionService questionService;
 
 	@GetMapping("/list")
-	public String list(Model model) {
-		List<Question> questionList = this.questionService.getList();
-		model.addAttribute("questionList", questionList);
+	public String list(Model model,  @RequestParam(value="page", defaultValue="0") int page) {
+		Page<Question> paging = this.questionService.getList(page);
+		model.addAttribute("paging", paging);
 		return "question_list";
 	}
 
--- src/test/java/com/mysite/sbbstudy/SbbstudyApplicationTests.java
+++ src/test/java/com/mysite/sbbstudy/SbbstudyApplicationTests.java
@@ -4,10 +4,12 @@ import com.mysite.sbbstudy.answer.Answer;
 import com.mysite.sbbstudy.answer.AnswerRepository;
 import com.mysite.sbbstudy.question.Question;
 import com.mysite.sbbstudy.question.QuestionRepository;
+import com.mysite.sbbstudy.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,7 +25,8 @@ class SbbstudyApplicationTests {
 	private QuestionRepository questionRepository;
 	@Autowired
 	private AnswerRepository answerRepository;
-
+	@Autowired
+	private QuestionService questionService;
 
 	@Test
 	void testJpa() {
@@ -130,5 +133,14 @@ class SbbstudyApplicationTests {
 
 		assertEquals(1, answerList.size());
 		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
+
+	@Test
+	void testJpa12() {
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
 	}
 }
