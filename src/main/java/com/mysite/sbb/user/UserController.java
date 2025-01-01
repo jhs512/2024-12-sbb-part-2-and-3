6336cee92d4a7a28b8058d116c180a8928e1219c
davinyakma <poohcdv3652@naver.com>
2025-01-01 22:21:18 +0900

    feat: 로그인 기능 구현하기_URL 등록 및 로그인 템플릿 작성

--- src/main/java/com/mysite/sbb/user/UserController.java
+++ src/main/java/com/mysite/sbb/user/UserController.java
@@ -21,6 +21,11 @@ public class UserController {
         return "signup_form";
     }
 
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
+    }
+
     @PostMapping("/signup")
     public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
