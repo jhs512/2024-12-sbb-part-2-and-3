0cd46b3e06eb030ac744c967f13742540174bafb
Gonhub9 <ho_gok@naver.com>
2025-01-08 15:19:03 +0900

    paging

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,33 +1,22 @@
 package com.mysite.sbb;
 
-import com.mysite.sbb.Question.Question;
-import com.mysite.sbb.Question.QuestionRepository;
+import com.mysite.sbb.Question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDateTime;
 
 @SpringBootTest
 class SbbApplicationTests {
 
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 
 	@Test
 	void TestJpa() {
-		// 첫번 째 질문 저장
-		Question q1 = new Question();
-		q1.setSubject("sbb가 뭔고?");
-		q1.setContent("sbb가 궁금합니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
-
-		// 두번 째 질문 저장
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문임");
-		q2.setContent("자동으로 id 생성됨?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트데이터:[03%d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
 	}
 }
