2a48cfdd87035922f8d36575e2cd1a49a7f90e39
Gonhub9 <ho_gok@naver.com>
2025-01-08 16:48:02 +0900

    signup

---
+++ src/main/java/com/mysite/sbb/User/SiteUser.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb.User;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    @Email
+    private String email;
+}
---
+++ src/main/java/com/mysite/sbb/User/UserRepository.java
@@ -0,0 +1,8 @@
+package com.mysite.sbb.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+
+}
+
