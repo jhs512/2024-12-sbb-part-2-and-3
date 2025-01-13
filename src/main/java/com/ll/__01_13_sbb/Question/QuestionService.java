3775d9dc22457930204e09750f70eaa1dc790a3d
gorhkd <gorhkd223@gmail.com>
2025-01-13 14:41:32 +0900

    2-9 service

---
+++ src/main/java/com/ll/__01_13_sbb/Question/QuestionService.java
@@ -0,0 +1,17 @@
+package com.ll.__01_13_sbb.Question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return questionRepository.findAll();
+    }
+}
