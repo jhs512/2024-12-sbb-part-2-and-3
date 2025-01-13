333b69d1b6693dfd5f71bab515c5aae34041ddb7
gorhkd <gorhkd223@gmail.com>
2025-01-13 17:53:34 +0900

    3-7 login

--- src/main/java/com/ll/__01_13_sbb/User/UserRepository.java
+++ src/main/java/com/ll/__01_13_sbb/User/UserRepository.java
@@ -3,6 +3,9 @@ package com.ll.__01_13_sbb.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<SiteUser, Integer> {
+    Optional<SiteUser> findByusername(String username);
 }
